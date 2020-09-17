0824820e036faa628df949badb34c7843b37f087
Mark <markpnoi26@gmail.com>
2020-09-16 19:45:49 -0700

    added a component score-board

---
+++ src/containers/ScoreBoard/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+const ScoreBoardContainer = props => {
+
+
+    return (
+        <div className="score-board">
+            I am a scoreboard
+        </div>
+    )
+
+}
+
+export default ScoreBoardContainer
