097710a6eeef4e9a85ea68edb74da5d775f9de7a
Mark <markpnoi26@gmail.com>
2020-09-16 19:46:19 -0700

    added main container

---
+++ src/containers/Main/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const MainContainer = (props) => {
+
+    return (
+        <div>
+            This is the main application container, we will be using hooks with this application.
+        </div>
+    )
+}       
+
+export default MainContainer 
